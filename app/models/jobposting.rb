a8cbd4155fbf50ff71c8dde2aa3fe1edea452b75
Yoan Ante <yoanante@gmail.com>
2015-11-29 20:12:28 -0500

    Added validations

---
+++ app/models/jobposting.rb
@@ -0,0 +1,10 @@
+class Jobposting < ActiveRecord::Base
+  validates  :location, presence: true
+  validate   :title, presence: true
+  validates  :description, presence: true
+  validates  :rate_of_pay, presence: true
+  validates  :type_of_pay, presence: true
+  validates  :user, presence: true
+  belongs_to :user
+  has_many :messages
+end
