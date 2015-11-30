0dbce565164124c02fc38fcd471554a9d6390262
Yoan Ante <yoanante@gmail.com>
2015-11-29 20:43:51 -0500

    added session controller and static loging / signup

---
+++ app/controllers/session.rb
@@ -0,0 +1,32 @@
+class SessionController < ApplicationController
+    # skip_before_action :verify_authenticity_token
+
+  def create
+    user = User.find_by(email: user_params[:email])
+    if user && user.authenticate(user_params[:password])
+      session[:current_user_id] = user.id
+      flash[:message] = "Thanks for logging in " + user.first_name.capitalize +
+      " "+user.last_name.capitalize[0]+ "."
+          redirect_to static_index_path
+          return
+    else
+      flash[:message] = "Username or Password combo are not correct"
+    end
+    redirect_to root_path
+  end
+
+  def the_current_user
+    current_user
+  end
+
+  def destroy
+    session[:current_user_id] = nil
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :password)
+  end
+end
