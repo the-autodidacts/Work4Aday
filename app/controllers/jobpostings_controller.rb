de563d84210514e17dd15deb2ee36d86d478fe9c
Yoan Ante <yoanante@gmail.com>
2015-11-29 20:22:34 -0500

    Added actions to jobposting controller

--- app/controllers/jobpostings_controller.rb
+++ app/controllers/jobpostings_controller.rb
@@ -1,2 +1,39 @@
 class JobpostingsController < ApplicationController
+  before_action :require_current_user
+  # skip_before_action :verify_authenticity_token
+
+  def index
+    @jobpostings = jobposting.all
+  end
+
+  def new
+  end
+
+  def create
+    @jobposting   = current_user.jobpostings.new(jobposting_params)
+    if @jobposting.save
+      flash[:message] = "Posting saved successfully"
+    else
+      flash[:message] = "error"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+    @jobpostings = Jobposting.all
+  end
+
+  private
+  def jobposting_params
+    params.require(:jobposting).permit(:location, :latitude, :longitude, :title, :description, :start_date, :start_time, :length, :rate_of_pay, :expires)
+  end
+
 end
